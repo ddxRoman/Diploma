02a69f7f180e56245722628bbb4ab444d5ecb0dc
ddxRoman <ddxman@mail.ru>
2023-06-23 18:59:02 +0300

    Логирование неудачных авторизаций с записью ip

--- action/signin.php
+++ action/signin.php
@@ -3,7 +3,7 @@ session_start();
  require_once 'connect.php';
 $login=$_POST['login'];
 $password=$_POST['password'];
-$check_admin = mysqli_query($connect, "SELECT * FROM `users` WHERE `name`='$login' AND `password` = '$password' ");
+$check_admin = mysqli_query($connect, "SELECT * FROM `users` WHERE `email`='$login' AND `password` = '$password' ");
 $check_user = mysqli_query($connect, "SELECT * FROM `personal` WHERE `mail`='$login' AND `password` = '$password' ");
 
 if(mysqli_num_rows($check_admin)>0) 
@@ -33,13 +33,30 @@ else{     if(mysqli_num_rows($check_user)>0)
         "photo"=>$user['photo'],
         "post"=>$user['post']
     ];
-
       header('Location: ../index.php');
-
 }
 else{
+    function getIp() {
+        $keys = [
+          'HTTP_CLIENT_IP',
+          'HTTP_X_FORWARDED_FOR',
+          'REMOTE_ADDR'
+        ];
+        foreach ($keys as $key) {
+          if (!empty($_SERVER[$key])) {
+            $ip = trim(end(explode(',', $_SERVER[$key])));
+            if (filter_var($ip, FILTER_VALIDATE_IP)) {
+              return $ip;
+            }
+          }
+        }
+      }
+      $ip = getIp();
+    mysqli_query($connect, "INSERT INTO `log` (`id`, `err`, `login`, `password`, `ip`)
+    VALUES (NULL, 'Попытка авторизации', '$login','$password', '$ip')");
     $_SESSION['sms']='Не верный <br> логин или пароль';
     header('Location: autorization.php');
+
 }
 }
 ?>
