664b7f72f667e0d2fc08416e7cc4502e2a605e40
DDX <ddxman@mail.ru>
2025-11-01 09:39:07 +0300

    Стилизация формы ТГ бота

--- folders/TgBotForm.php
+++ folders/TgBotForm.php
@@ -4,11 +4,37 @@ else{$last_day_holidays=$_SESSION['holidays_day'];}
 
 ?>
 
-<link rel="stylesheet" href="../css/adminStyle.css">
+<style>
+    .input_TG{
+        width: 50%;
+        height: 35px;
+        margin-bottom: 3%;
+    }
+    .input_TG_hooliday{
+        width: 35%;
+        height: 35px;
+        margin-bottom: 3%;
+
+    }
+    .input_TG_date{
+        width: 15%;
+        height: 35px;
+    }
+    .Tg-Block-Form{
+padding-left: 10%;
+    }
+    textarea{
+        width: 70%;
+        height: 100px;
+        margin-bottom: 5%;
+    }
+</style>
+
+
 <div class="Tg-Block-Form">
     <form action="../bot/bot.php" method="post">   
-        <input required class="input_TG" name="data" type="date" value="<?= $last_day_holidays ?>" autofocus>
-        <input required class="input_TG" type="text" name="holiday" placeholder="Праздник"><br>
+        <input required class="input_TG_date" name="data" type="date" value="<?= $last_day_holidays ?>" autofocus>
+        <input required class="input_TG_hooliday" type="text" name="holiday" placeholder="Праздник"><br>
         <input class="input_TG" type="text" name="description" placeholder="Подпись"><br>
         <input class="input_TG" type="text" name="url" placeholder="Ссылка на картинку"><br>
         <textarea class="txtArea_TG"  name="comments" placeholder="Комментарий"></textarea><br>
